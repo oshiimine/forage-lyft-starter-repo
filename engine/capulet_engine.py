5c8ae60448bf67c7fba146bc69ec04c4955903f4
Alex Dai <60891469+oshiimine@users.noreply.github.com>
2023-07-12 21:20:57 -0400

    Fixed engine services checkers

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -1,10 +1,14 @@
 from engine.engine import Engine
 from constants import Constants
 
+
 class CapuletEngine(Engine):
     def __init__(self, current_mileage, last_service_mileage):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
-    def engine_should_be_serviced(self):
-        return self.current_mileage - self.last_service_mileage > Constants.capuletMilageRequirement
+    def needs_service(self):
+        return (
+            self.current_mileage - self.last_service_mileage
+            > Constants.capuletMilageRequirement
+        )
--- engine/willoughby_engine.py
+++ engine/willoughby_engine.py
@@ -1,10 +1,14 @@
 from engine.engine import Engine
 from constants import Constants
 
+
 class WilloughbyEngine(Engine):
     def __init__(self, current_mileage, last_service_mileage):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
-    def engine_should_be_serviced(self):
-        return self.current_mileage - self.last_service_mileage > Constants.willoughbyMilageRequirement
+    def needs_service(self):
+        return (
+            self.current_mileage - self.last_service_mileage
+            > Constants.willoughbyMilageRequirement
+        )
